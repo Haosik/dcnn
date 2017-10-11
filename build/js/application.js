214b7c0799aba99cc6f854b828d9b288fa18c2da
Haosik <noobbaster@gmail.com>
2017-10-11 05:55:33 +0300

    adaptive + ajax handler

--- build/js/application.js
+++ build/js/application.js
@@ -8,4 +8,23 @@ $(document).ready(function() {
     $('html, body').stop().animate({ scrollTop: commentsOffTop }, 400)
   })
 
+  // Ajax Handler
+  var ajaxRequest = function(options) {
+    var url = options.url || '/';
+    var method = options.method || 'GET';
+    var callback = options.callback || function() {};
+    var data = options.data || {};
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.open(method, url, true);
+    xmlHttp.setRequestHeader('Content-Type', 'application/json');
+    xmlHttp.send(JSON.stringify(data));
+
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.status === 200 && xmlHttp.readyState === 4) {
+        callback(xmlHttp.responseText);
+      }
+    };
+  };
+
 });
