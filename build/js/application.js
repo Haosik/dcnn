a7f656afa5384376c085d50469f5319c1d20c471
Haosik <noobbaster@gmail.com>
2017-10-11 05:14:07 +0300

    full desktop markup

--- build/js/application.js
+++ build/js/application.js
@@ -1,4 +1,11 @@
 ;
 $(document).ready(function() {
 
+  // Scroll to comments
+  var commentsOffTop = $('.comments-wrap').offset().top;
+  $('.article-info__comments').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').stop().animate({ scrollTop: commentsOffTop }, 400)
+  })
+
 });
